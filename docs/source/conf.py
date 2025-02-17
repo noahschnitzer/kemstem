23e31f4df604d4158322f9a5da8e39b58ef6f877
Noah Schnitzer <nschnitz@umich.edu>
2025-02-17 16:42:20 -0500

    bump to 0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'kemstem'
 copyright = '2025, Noah Schnitzer'
 author = 'Noah Schnitzer'
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
